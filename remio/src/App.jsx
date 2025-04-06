e701e5bd64884f38ce7b2f4570d3b58a5c28afa5
Shemaremy <remyshema20@gmail.com>
2025-04-06 15:04:32 +0200

    Did the setup

---
+++ remio/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Part1 from './components/Part1'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Part1 />
+    </div>
+  )
+}
+
+export default App
